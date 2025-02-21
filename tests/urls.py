d11f9865d75bc343f9016e00a7da7813d96cac65
che-hyeon <ghkdcogus03@naver.com>
2025-02-21 17:25:18 +0900

    Feat: is_test

--- tests/urls.py
+++ tests/urls.py
@@ -4,13 +4,17 @@ from rest_framework.routers import DefaultRouter
 
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import TotaltestViewSet
+from .views import TotaltestViewSet, TestCheckView
 
 app_name = "tests"
 
 router = DefaultRouter()
 router.register('', TotaltestViewSet, basename='score')
 
+default_router = routers.SimpleRouter(trailing_slash=False)
+default_router.register('check', TestCheckView, basename='check')
+
 urlpatterns = [
     path('', include(router.urls)),  # 라우터 URL 포함
+    path('', include(default_router.urls))
 ]
