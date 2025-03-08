0408e7084fcd6830504e4df44ba5f8e782f68859
che-hyeon <ghkdcogus03@naver.com>
2025-03-09 00:33:11 +0900

    Feat: is_read 제대로git add .

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,14 +4,13 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password', 'user_name', 'phone']
+        fields = ['email', 'password', 'user_name']
 
     def create(self, validated_data):
         user = User.objects.create_user(
             email = validated_data['email'],
             password = validated_data['password'],
-            user_name = validated_data.get('user_name', ''),
-            phone = validated_data.get('phone', '')
+            user_name = validated_data.get('user_name', '')
         )
         user.save()
         return user
