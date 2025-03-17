248d36d9e0e436a8a7358820cde8b6a181e85070
che-hyeon <ghkdcogus03@naver.com>
2025-03-17 14:21:02 +0900

    Fix: 약속 잡기 오류 해결

--- chat/routing.py
+++ chat/routing.py
@@ -4,4 +4,5 @@ from chat import consumers
 
 websocket_urlpatterns = [
     path("ws/room/<int:room_id>/messages", consumers.ChatConsumer.as_asgi()),
+    path("ws/user/chatrooms", consumers.UserChatConsumer.as_asgi()),
 ]
